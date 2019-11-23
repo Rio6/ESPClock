a5d56cdc12f66fb4fec51f7ed5f2a4b008b1808c
Rio6 <rio6.liu@gmail.com>
2019-11-23 14:25:36 -0800

    Secret.h template

---
+++ include/secret.h
@@ -0,0 +1,6 @@
+#pragma once
+#define WIFI_SSID "wifi-ssid"
+#define WIFI_PASSWORD "wifi-password"
+#define WEATHER_API_KEY "openweathermap.org-api-key"
+#define WEATHER_CITY_ID "city-id"
+#define TIME_ZONE "UTC0"
